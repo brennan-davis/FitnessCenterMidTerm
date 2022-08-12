0c6133355c93167990c8ba79bc689d025d14b022
Tolu <tsorinmade@yahoo.com>
2022-08-11 21:03:59 -0400

    Added Club Class

---
+++ Classes/Club.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FitnessCenterMidTerm.Classes
+{
+    public class Club
+    {
+        public Club(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        //Properties
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
+
