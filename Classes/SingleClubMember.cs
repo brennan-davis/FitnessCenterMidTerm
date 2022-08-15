7bfd4ad6485018e58c3d49794918fe3b08f7a299
Tolu <tsorinmade@yahoo.com>
2022-08-15 17:46:53 -0400

    Added SingleClubMember Class & CheckInMethod

---
+++ Classes/SingleClubMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FitnessCenterMidTerm.Classes
+{
+    public class SingleClubMember : Member
+    {
+        public bool clubMembership { get; set; }
+
+        public override void CheckIn(Club club)
+        {
+
+            if (clubMembership == true)
+            {
+                Console.WriteLine($"Welcome {Name}");
+            }
+
+            else
+            {
+                throw new Exception("You are currently not a member of this club.Would you like to join ?");
+            }
+        }
+    }
+}
+
