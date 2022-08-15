f2f0fbee73855e4f98fe507980bbfae7d88aa60b
Tolu <tsorinmade@yahoo.com>
2022-08-15 17:52:15 -0400

    Added MultiClubMemberClass and CheckIn Method

---
+++ Classes/MultiClubMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FitnessCenterMidTerm.Classes
+{
+    public class MultiClubMember : Member
+    {
+        public MultiClubMember()
+        {
+            MemberCheckinPoints = 12;
+        }
+
+        public int MemberCheckinPoints { get; set; }
+
+        public override void CheckIn(Club club)
+        {
+            int TotalMembershipPoints = 0;
+            Console.WriteLine(TotalMembershipPoints += MemberCheckinPoints);
+        }
+    }
+}
+
