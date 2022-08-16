2f3b5ba3e1a1ae1ebd6865dfc29cb8dc4e99bc92
Tolu <tsorinmade@yahoo.com>
2022-08-15 20:26:32 -0400

    Corrected Formating Error

--- Classes/MultiClubMember.cs
+++ Classes/MultiClubMember.cs
@@ -15,19 +15,21 @@ namespace FitnessCenterMidTerm.Classes
         public int MemberPoints { get; set; }
 
         // Methods
+
+        //Method to check in
         public override void CheckIn(Club club)
         {
             MemberPoints += 1;
             Console.WriteLine(MemberPoints);
         }
 
+        //Method to display member info
         public override string ToString()
         {
             string a = String.Format("{0,-10} {1, -10}", "ID:", $"{Id}");
             string b = String.Format("\n{0,-10} {1, -10}", "Model:", $"{Name}");
             string c = String.Format("\n{0,-10} {1, -10}", "Club:", "All");
-            string d = String.Format("\n{0,-10} {1, -10}", "Membership Points:", $"${MemberPoints}");
-          
+            string d = String.Format("\n{0,-10} {1, -10}", "Membership Points:", $"{MemberPoints}");
             return a + b + c + d;
         }
     }
