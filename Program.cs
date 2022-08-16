58d45feb5efb63034126074bbf85bf93363eee67
Lisa Van Huijkelom <lisa.vanhuijkelom@infinicept.com>
2022-08-15 18:26:24 -0600

    adding members

--- Program.cs
+++ Program.cs
@@ -2,7 +2,17 @@
 List<Club> clubs = Admin.GetClubList();
 List<SingleClubMember> singleClubMembers = new List<SingleClubMember>()
 {
-    new SingleClubMember(2,"Lisa", clubs[0])
+    new SingleClubMember(2,"Lisa", clubs[0]),
+    new SingleClubMember(3, "Tolu", clubs[1])
+};
+Console.WriteLine(singleClubMembers[0].ClubMembership.Name);
+
+singleClubMembers.ForEach(member => member.CheckIn(clubs[1]));
+
+List<MultiClubMember> MultiClubMember = new List<MultiClubMember>()
+{
+    new MultiClubMember(4,"Brennan"),
+    new MultiClubMember(5, "Morgan")
 };
 Console.WriteLine(singleClubMembers[0].ClubMembership.Name);
 
